785072b4a23747a863db0211afd2ba43266b1b96
--global <--local>
2021-07-12 00:01:42 -0300

    Agregar routing y placeholders de pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { LayoutModule } from "./layout/layout.module";
 
 @NgModule({
@@ -9,8 +10,9 @@ import { LayoutModule } from "./layout/layout.module";
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    LayoutModule
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule
