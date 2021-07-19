4bcf125f27854d5340420bb7178637f06d2ac801
famaya <famaya@encodesa.com.ar>
2021-07-19 17:51:45 -0300

    Se agregó el componente forget-pass para la parte de olvido de contraseña

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetPassComponent } from './pages/forget-pass/forget-pass.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'forget-pass', component: ForgetPassComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ForgetPassComponent } from './forget-pass/forget-pass.component';
 
 
 
@@ -10,7 +11,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
   declarations: [
     HomeComponent,
     LoginComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ForgetPassComponent
   ],
   imports: [
     CommonModule
