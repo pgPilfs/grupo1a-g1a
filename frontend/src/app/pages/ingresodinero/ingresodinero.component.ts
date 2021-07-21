ee48d73d45ed1dfede6b12cb277e5d9a96d89120
florcorvalan <florcorvalan26@gmail.com>
2021-07-20 23:03:09 -0300

    vista componente ingreso dinero terminada

--- frontend/src/app/pages/ingresodinero/ingresodinero.component.ts
+++ frontend/src/app/pages/ingresodinero/ingresodinero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingresodinero',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresodineroComponent implements OnInit {
 
+  mostrar:Boolean = false;
+  lugares:Boolean=true;
+  monto = new FormControl('', Validators.required);
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  mostrarOcultar(){
+    if(this.mostrar){
+      this.mostrar=false;
+    }else{
+      this.mostrar=true;
+      this.lugares=false;
+    }
   }
 
 }
